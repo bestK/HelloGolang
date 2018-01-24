db2c5eec338b673be6345e39b14ee915859d541c
ECH2O <1995kaikai@gmail.com>
2018-01-24 16:13:06 +0800

    [del] 删除无用代码

--- controller/userController.go
+++ controller/userController.go
@@ -10,20 +10,6 @@ import (
 /*
 	数据库查询
 */
-func SelectUser_(c echo.Context) error {
-	curPage := c.QueryParam("curPage")
-	pageSize := c.QueryParam("pageSize")
-
-	if curPage == "" || pageSize == "" {
-		return c.JSON(http.StatusBadRequest, util.Fr(http.StatusBadRequest, "parameter invalid", nil))
-	}
-
-	return c.JSON(http.StatusOK, util.Fr(http.StatusOK, "successfully", util.Querys("SELECT id,nick_name FROM vino_user LIMIT "+curPage+" , "+pageSize, nil)))
-}
-
-/*
-	数据库查询
-*/
 func SelectUser(c echo.Context) error {
 
 	data, err := service.SelectUsers(c)
